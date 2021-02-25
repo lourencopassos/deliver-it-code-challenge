43371d641abbd463b2ad2a6f3fddbe70d5e52f6c
lourencopassos <lourenco.passos@somosphi.com>
2021-02-25 00:03:04 -0300

    chore: create bill model

--- backend/src/model/bill.ts
+++ backend/src/model/bill.ts
@@ -2,5 +2,34 @@ import mongoose, { Schema } from 'mongoose';
 
 
 export const BillSchema = new Schema({
-  
+  name: {
+    type: String,
+    required: 'Bill name required. At least 10 characters.',
+    min: 10
+  },
+  price: {
+    type: Number,
+    required: 'Bill price required',
+    min: 0.01,
+  },
+  due_date: {
+    type: Date,
+    required: true,
+    min: Date.now()
+  },
+  paid_at: {
+    type: Date,
+  },
+  overdue_days: {
+    type: Number
+  }
+
 })
+
+export type BillInputDTO = {
+  price: number,
+  name: string,
+  due_date: Date
+}
+
+export const BillModel = mongoose.model('bills', BillSchema);
